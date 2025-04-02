51333bd45a6220ec6daf974febdd1de1754ac3ee
[Kkimdoyeon] <[ehdus5176@naver.com]>
2025-04-02 20:07:27 +0900

    백준_18258

--- src/main/java/algo/ct/BOJ18258.java
+++ src/main/java/algo/ct/BOJ18258.java
@@ -5,13 +5,52 @@ import java.io.*;
 
 public class BOJ18258 {
 
-    static Queue<Integer> queue = new LinkedList<>();
-    static int num;
+    static Deque<Integer> queue = new LinkedList<>();
+    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        N = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int X = Integer.parseInt(command.split(" ")[1]);
+                queue.add(X);
+            }
+            else if (command.equals("pop")) {
+                if (queue.isEmpty())
+                    sb.append(-1).append("\n");
+                else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            }
+            else if (command.equals("size")) {
+                sb.append(queue.size()).append("\n");
+            }
+            else if (command.equals("empty")) {
+                if (queue.isEmpty())
+                    sb.append(1).append("\n");
+                else
+                    sb.append(0).append("\n");
+            }
+            else if (command.equals("front")) {
+                if (queue.isEmpty())
+                    sb.append(-1).append("\n");
+                else
+                    sb.append(queue.peek()).append("\n");
+            }
+            else if (command.equals("back")) {
+                if (queue.isEmpty())
+                    sb.append(-1).append("\n");
+                else
+                    sb.append(queue.peekLast()).append("\n");
+            }
+        }
+
+        System.out.print(sb);
     }
 }
