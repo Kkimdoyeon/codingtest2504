6f76e909c15e24b58b40491d1eb2fd21a0e5e75f
[Kkimdoyeon] <ehdus5176@gmail.com>
2025-04-05 15:47:23 +0900

    백준_2164

---
+++ src/main/java/algo/ct/BOJ_2164.java
@@ -0,0 +1,28 @@
+package main.java.algo.ct;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2164 {
+    static int N;
+    static Queue<Integer> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        sb.append(queue.poll());
+        System.out.print(sb);
+    }
+}
