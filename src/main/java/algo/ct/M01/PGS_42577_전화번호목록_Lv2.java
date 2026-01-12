34d96212f6dcaeebb27e9f02653607af56f2c442
[Kkimdoyeon] <ehdus5176@gmail.com>
2026-01-12 11:40:11 +0900

    PGS_42577_전화번호목록_Lv2

---
+++ src/main/java/algo/ct/M01/PGS_42577_전화번호목록_Lv2.java
@@ -0,0 +1,16 @@
+package algo.ct.M01;
+
+import java.util.Arrays;
+
+public class PGS_42577_전화번호목록_Lv2 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i]))
+                return false;
+        }
+
+        return true;
+    }
+}
